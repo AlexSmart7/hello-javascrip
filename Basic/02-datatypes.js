71f4696c93fcd1727db4f9103ef362fbddabf1e5
AlexCeja-157 <el.ktrin157@gmail.com>
2025-01-16 18:58:50 -0600

    Tipos de Datos

---
+++ Basic/02-datatypes.js
@@ -0,0 +1,62 @@
+//?  Tipos de Datos Primitivos
+
+
+//? String / Cadena de Texto
+
+    let name = "Alex Ceja"
+    let alias = 'Ktrin157'
+    let email = `el.ktrin157@gmail.com`
+
+//? Numeros (number)
+
+    let age = 37 //Entero
+    let heingt = 1.65 // Decimal
+
+//? Booleanos (boolean)
+
+    let isStudent = true
+    let isTeacher = false
+
+//? Undefined
+
+    let undefinedValue
+    //console.log(undefinedValue)
+
+
+//? Null
+
+    let nullValue = null
+    //console.log(nullValue)
+
+//? Symbol 
+
+    let mySymbol = Symbol("mysymbol")
+
+//? BigInt
+
+    let myBigInt = BigInt(556546545645487842313217768721315464654654654)
+    let myBigInt2 = 8754212345678441321378454135247384544533432n
+
+
+
+//? Mostramos los Tipos de Datos
+
+    console.log(typeof name)
+    console.log(typeof alias)
+    console.log(typeof email)
+
+    console.log(typeof age)
+    console.log(typeof heingt)
+
+    console.log(typeof isStudent)
+    console.log(typeof isTeacher)
+
+    console.log(typeof undefinedValue)
+
+    console.log(typeof nullValue)
+
+    console.log(typeof mySymbol)
+
+    console.log(typeof myBigInt)
+    console.log(typeof myBigInt2)
+
