c6b1627fe9c3e5e9d6a431ec942ccc222f91ca7e
AlexCeja-157 <el.ktrin157@gmail.com>
2025-01-16 19:25:27 -0600

    Ejercicios 1 Resueltos

---
+++ Basic/03-beginner-excercises.js
@@ -0,0 +1,77 @@
+//? 1. Escribe un comentario en una linea
+
+    // Comentario simple sbre una linea
+
+//? 2.Escribe un comentario en varias lineas
+
+    /* Comentario
+    sobre varias 
+    lineas de comando
+    */
+
+//? 3.Declara variables con valores asociados a todos los datos de tipo primitivos
+
+    //Variables con datos Primitivos
+
+    let nombre = 'Sthefy'
+    let edad = 33
+    let esMama = true
+    let undefinedVal
+    let nullVal = null
+    
+
+//? 4.Imprime por consola el valor de todas las variables
+
+    console.log(nombre)
+    console.log(edad)
+    console.log(esMama)
+    console.log(undefinedVal)
+    console.log(nullVal)
+
+//? 5.Imprime por consola el tipo de todas las variables
+
+    console.log(typeof nombre)
+    console.log(typeof edad)
+    console.log(typeof esMama)
+    console.log(typeof undefinedVal)
+    console.log(typeof nullVal)
+
+//? 6.A continuacion, modifica los valores de las variables por otros del mismo tipo
+
+    nombre = 'Chris'
+    console.log(nombre)
+    edad = 4
+    console.log(edad)
+    esMama = false
+    console.log(esMama)
+
+//? 7.A continuacion, modifica los valores de las variables por otros de distinto tipo
+
+    nombre = 3
+    console.log(nombre)
+    edad = "niño"
+    console.log(edad)
+    esMama = "Incorrecto"
+    console.log(esMama)
+
+//? 8.Decalara constantes con valores asociados a todos los tipo de datos primitivos
+
+    const name = 'Santy'
+    console.log(name)
+    const age = 12
+    console.log(age)
+    const isMoomy = false
+    console.log(isMoomy)
+
+//? 9. A continuacion, modifica los valores de las constantes
+
+/*
+    name = 2
+    console.log(name)
+    age = "Adolecente"
+    console.log(age)
+    isMoomy = "Incorrecto"
+    console.log(isMoomy)
+*/
+
+//? 10.Comenta las lineas que produzcan algun tipo de error al ejecutarse
